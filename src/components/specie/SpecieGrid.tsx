4c3cfca76bd93cd28726f0cc3c4d68164d35f6d4
Dwigth Astacio <dwigth32@outlook.es>
2023-06-19 16:30:01 -0600

    fix (deploy): remove unused imports

--- src/components/specie/SpecieGrid.tsx
+++ src/components/specie/SpecieGrid.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { Box, Button, Grid } from "@mui/material";
 import { SPECIES } from '../../data/species';
 import { SelectedSpecie } from '../../data/types/specie.type';
